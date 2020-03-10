74b868047d42244c47f8f61f0498f3f37d3ffa8f
Nicodemus <nicodemus.maurits@gmail.com>
2020-03-10 19:40:01 +0100

    Add error handler

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,27 @@ app.use(
   })
 );
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World"
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "development" ? error.stack : ""
+  });
+});
+
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
